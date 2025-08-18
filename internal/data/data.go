cb8a8cbb69b326d48e5fba5e8faa11db3c3dabaf
life00 <contact.5lu1v@simplelogin.co>
2025-08-18 10:13:11 +0200

    add validateExchanges()

--- internal/data/data.go
+++ internal/data/data.go
@@ -1,19 +1,17 @@
 package data
 
 import (
-	"errors"
 	"fmt"
-	"log/slog"
 
 	"github.com/life00/arbitrage-inspector/internal/models"
 )
 
 func FetchData(exchanges models.Exchanges, currencies models.Currencies) error {
-	if false {
-		err := errors.New("something went wrong")
-		slog.Error("something went wrong")
+	err := validateExchanges(exchanges)
+	if err != nil {
 		return err
 	}
+
 	fmt.Println(exchanges, currencies)
 	return nil
 }
--- internal/data/exchange.go
+++ internal/data/exchange.go
@@ -1,3 +1,21 @@
 package data
 
-// low-level interactions with CCXT
+import (
+	"errors"
+	"fmt"
+	"log/slog"
+
+	"github.com/ccxt/ccxt/go/v4"
+	"github.com/life00/arbitrage-inspector/internal/models"
+)
+
+func validateExchanges(exchanges models.Exchanges) error {
+	if false {
+		err := errors.New("something went wrong")
+
+		slog.Error("something went wrong")
+		return err
+	}
+	fmt.Println(ccxt.Exchanges)
+	return nil
+}
