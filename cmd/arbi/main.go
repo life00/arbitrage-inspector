a327bf5fd481901352f8e50c3d7444ce90e7e946
life00 <contact.5lu1v@simplelogin.co>
2025-09-01 11:58:44 +0200

    Revert "refactor(arbi): restructure for arbitrage development"
    This reverts commit 61925302af7a735f2e76d93a22bf507a1bf41f3e.

--- cmd/arbi/main.go
+++ cmd/arbi/main.go
@@ -5,10 +5,10 @@ import (
 	"log/slog"
 	"os"
 
-	"github.com/govalues/decimal"
-	// "github.com/joho/godotenv"
-	"github.com/life00/arbitrage-inspector/internal/arbitrage"
-	// "github.com/life00/arbitrage-inspector/internal/data"
+	// "github.com/govalues/decimal"
+	"github.com/joho/godotenv"
+	// "github.com/life00/arbitrage-inspector/internal/arbitrage"
+	"github.com/life00/arbitrage-inspector/internal/data"
 	// "github.com/life00/arbitrage-inspector/internal/models"
 )
 
@@ -26,69 +26,66 @@ func main() {
 	slog.Info("successfully started logger")
 
 	// get the environment variables (API credentials)
-	// err := godotenv.Load()
-	// if err != nil {
-	// 	slog.Error("failed to load .env file")
-	// 	os.Exit(1)
-	// }
+	err := godotenv.Load()
+	if err != nil {
+		slog.Error("failed to load .env file")
+		os.Exit(1)
+	}
 
 	// TODO: Parse cli arguments and define inputs
-
-	// inputExchanges := []string{
-	// 	"binance",
-	// 	"kucoin",
-	// 	"bitget",
-	// 	"htx",
-	// 	"coinbase",
-	// }
-	// inputCurrencies := []string{
-	// 	"BTC",
-	// 	"ETH",
-	// 	"USDC",
-	// 	"DOGE",
-	// 	"SOL",
-	// 	"BNB",
-	// 	"USDT",
-	// 	"BCH",
-	// 	"LTC",
-	// 	"XMR",
-	// }
+	inputExchanges := []string{
+		"binance",
+		"kucoin",
+		"bitget",
+		// "htx",
+		// "coinbase",
+	}
+	inputCurrencies := []string{
+		"BTC",
+		"ETH",
+		"USDC",
+		"DOGE",
+		"SOL",
+		"BNB",
+		"USDT",
+		"BCH",
+		"LTC",
+	}
 
 	// 1. Data retrieval using data.go, exchange.go
 	// 1.1. Validating and transforming the inputs; initializing the library
-	// exchanges, clients, err := data.InitializeExchanges(inputExchanges, inputCurrencies)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	os.Exit(1)
-	// }
-
-	// 1.2. Fetching price data and fees
-
-	// err = data.UpdateExchanges(&exchanges, &clients, true, true)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	os.Exit(1)
-	// }
-
-	exchanges, err := loadExchanges("exchanges.json")
+	exchanges, clients, err := data.InitializeExchanges(inputExchanges, inputCurrencies)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	fmt.Println(exchanges)
+
+	// 1.2. Fetching price data and fees
+
+	err = data.UpdateExchanges(&exchanges, &clients, true, true)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	err = saveExchanges(exchanges, "exchanges.json")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
 	// 2. Arbitrage identification using arbitrage.go
 	// 2.1. Transforming data
 
-	capital, err := decimal.NewFromFloat64(100)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	assets, pairs := arbitrage.GenerateAssetPairs(&exchanges, capital)
-
-	fmt.Println(pairs, assets)
+	// capital, err := decimal.NewFromFloat64(100)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	os.Exit(1)
+	// }
+	//
+	// assets, pairs := arbitrage.GenerateAssetPairs(&exchanges, capital)
+	//
+	// fmt.Println(pairs, assets)
 
 	// 2.2. Bellman-Ford algorithm negative cycle detection
 
